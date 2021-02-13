c887d6d27f79ea13b819c47f9cd7dc0158de1c27
rohanmalik <rohan_malik@ryecountryday.org>
2021-02-13 11:47:34 -0500

    Overhaul of game logic and other improvements

--- LockbusterPrototype/ContentView.swift
+++ LockbusterPrototype/ContentView.swift
@@ -11,7 +11,7 @@ struct ImageAnimated: UIViewRepresentable {
     let imageSize: CGSize
     let group: Int
     let lock: Int
-    let duration: Double = 0.7
+    let duration: Double = 0.6
 
     func makeUIView(context: Self.Context) -> UIView {
         let imageNames = (1...13).map { "G\(group)L\(lock)F\($0)" }
@@ -47,47 +47,145 @@ struct ImageAnimated: UIViewRepresentable {
     }
 }
 
-func chooseGesture() -> String {
-    let gestures: [String] = ["Tap", "Left Swipe", "Up Swipe", "Down Swipe", "Right Swipe", "Rotate", "Pinch", "Expand"]
-    return gestures[Int.random(in: 0...7)] // change upper bound depending on number of gestures
+
+struct NamedGesture {
+    let gestureName: String
+    let gesture: Any
 }
 
-// TODO: figure out directional gestures; investigate multi-finger tap or drag; implement varied gestures in recognizer and main state updater
-
 struct ContentView: View {
-    @State var started: Bool = true
+    @State var started: Bool = false
     @State var timeLeft: Float = 60.0
+    @State var currentLock = 1
+    @State var currentGesture: NamedGesture = NamedGesture(gestureName: "Tap", gesture: TapGesture())
+    @State var currentGestureName: String = "Double Tap"
     @State var currentGestureDone = false
+    var isAnimating = false
     var body: some View {
-        VStack {
-            if !started {  // welcome text
+        return VStack {
+            if !started {  // "welcome screen"
                 Text("Welcome to Lockbuster!")
-                Button("Start Game", action: {self.started = true})
+                Button("Start Game", action: {
+                    print("starting game")
+                    self.started = true
+                })
             }
             else {
-                Text("Tap")
-                    .padding(.bottom)
-                    .scaleEffect(2)
+                //Text(currentGesture.gestureName).padding(.bottom).scaleEffect(2)
+                Text(currentGestureName).padding(.bottom).scaleEffect(2)
                 if currentGestureDone {
-                    ImageAnimated(imageSize: CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5), group: 1, lock: 1)
-                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5, alignment: .center)
+                    animateLock()
                 }
                 else {
-                    Image("G1L1F1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5, alignment: .center)
-                        .gesture(
-                            TapGesture(count: 2)
-                                .onEnded { _ in
-                                    currentGestureDone = true
-                                }
-                        )
+                    createLock()
                 }
             }
         }
     }
+    
+    func animateLock() -> some View {
+        //self.currentGesture = chooseGesture()
+        print("animating lock")
+        // self.currentGestureDone = false
+        return ImageAnimated(imageSize: CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5), group: 1, lock: currentLock)
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5, alignment: .center)
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now()+0.7, execute: {self.currentLock = Int.random(in: 1...5); self.currentGestureDone = false})
+            })
+    }
+    
+    /*
+    func showLock() -> some View {
+        //self.currentLock = Int.random(in: 1...5)
+        if self.currentGesture.gestureName.contains("Two") {
+            let g = self.currentGesture.gesture as! SimultaneousGesture<TapGesture, TapGesture>
+            return AnyView(Image("G1L\(self.currentLock)F1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5, alignment: .center)
+                .gesture(g))
+        } else if self.currentGesture.gestureName.contains("Three") {
+            let g = self.currentGesture.gesture as! SimultaneousGesture<SimultaneousGesture<TapGesture, TapGesture>, TapGesture>
+            return AnyView(Image("G1L\(self.currentLock)F1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5, alignment: .center)
+                .gesture(g))
+        } else if self.currentGesture.gestureName.contains("Tap") {
+            let g = self.currentGesture.gesture as! TapGesture
+            return AnyView(Image("G1L\(self.currentLock)F1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5, alignment: .center)
+                .gesture(g))
+        } else if self.currentGesture.gestureName.contains("Pinch") {
+            let g = self.currentGesture.gesture as! MagnificationGesture
+            return AnyView(Image("G1L\(self.currentLock)F1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5, alignment: .center)
+                .gesture(g))
+        } else {
+            let g = self.currentGesture.gesture as! RotationGesture
+            return AnyView(Image("G1L\(self.currentLock)F1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5, alignment: .center)
+                .gesture(g))
+        }
+    }
+    */
+    
+    func createLock() -> some View {
+        print("creating lock")
+        let num = Int.random(in: 1...2)
+        if num == 1 {
+            print("chosen 2t")
+            //self.currentGestureName = "Double Tap"
+            return AnyView(Image("G1L\(self.currentLock)F1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5, alignment: .center)
+                            .gesture(TapGesture(count: 2).onEnded{self.currentGestureDone = true; print("2t")})
+                            .onAppear(perform: {
+                                self.currentGestureName = "Double Tap"
+                            })
+                            .animation(.easeInOut)
+            )
+        } else {
+            print("chosen 3t")
+            //self.currentGestureName = "Triple Tap"
+            return AnyView(Image("G1L\(self.currentLock)F1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/1.5, alignment: .center)
+                            .gesture(TapGesture(count: 3).onEnded{self.currentGestureDone = true; print("3t")})
+                            .onAppear(perform: {
+                                self.currentGestureName = "Triple Tap"
+                            })
+                            .animation(.easeInOut)
+            )
+        }
+        
+    }
+    
+    /*
+    func chooseGesture() -> NamedGesture {
+        let possibleGestures = [
+            NamedGesture(gestureName: "Double Tap", gesture: TapGesture(count: 2).onEnded {self.currentGestureDone = true; print("2t")} ),
+            NamedGesture(gestureName: "Rotate", gesture: RotationGesture().onEnded {_ in self.currentGestureDone = true; print("r")} ),
+            NamedGesture(gestureName: "Pinch", gesture: MagnificationGesture().onEnded {_ in self.currentGestureDone = true; print("p")} ),
+            NamedGesture(gestureName: "Triple Tap", gesture: TapGesture(count: 3).onEnded {self.currentGestureDone = true; print("3t")} ),
+            NamedGesture(gestureName: "Two Finger Tap", gesture: SimultaneousGesture(TapGesture(count: 1), TapGesture(count: 1)).onEnded {_ in self.currentGestureDone = true; print("2ft")} ),
+            NamedGesture(gestureName: "Three Finger Tap", gesture: SimultaneousGesture(SimultaneousGesture(TapGesture(count: 1), TapGesture(count: 1)), TapGesture(count: 1)).onEnded {_ in self.currentGestureDone = true; print("3ft")} )
+        ]
+        return possibleGestures[Int.random(in: 0...5)]
+    }
+    */
+    
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
