ac17fe449ef19c52161dfa24c8d678fbbae64b9d
rohanmalik <rohan_malik@ryecountryday.org>
2021-02-13 08:08:47 -0500

    Initial Commit

---
+++ LockbusterPrototype/LockbusterPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockbusterPrototypeApp.swift
+//  LockbusterPrototype
+//
+//  Created by Rohan MALIK on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LockbusterPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
